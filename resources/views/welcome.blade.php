cfbdfe125318fe8d6f9234c7451c3015cf3ab2ae
nakashi <coffeelike.ncnn@gmail.com>
2020-05-18 07:26:53 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <div class="center jumbotron">
         <div class="text-center">
-            <h1>飲んだコーヒーの味を覚えてますか？</h1>
+            <h1>一緒に楽しいコーヒーライフを！</h1>
+            <h3>飲んだコーヒー豆の情報を記録・共有するサイトです。</h3>
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
