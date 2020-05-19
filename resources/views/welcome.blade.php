77f44364b47bf1a0ed4b770e8166afc81380876b
nakashi <coffeelike.ncnn@gmail.com>
2020-05-19 02:34:18 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+    @if (Auth::check())
+        {{ Auth::user()->name }}
+    @else
     <div class="center jumbotron">
         <div class="text-center">
             <h1>一緒に楽しいコーヒーライフを！</h1>
             <h3>飲んだコーヒー豆の情報を記録・共有するサイトです。</h3>
-            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            {!! link_to_route('signup.get', '会員登録', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
+    @endif
 @endsection
