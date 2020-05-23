bbcb1f2712eb1094b7717ba6a268708feaeb6d1f
nakashi <coffeelike.ncnn@gmail.com>
2020-05-23 04:21:11 +0000

    ->profile_content?

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -8,7 +8,7 @@
         <div class="col-sm-8">
             <h3>プロフィール</h3>
         @if (Auth::id() == $user->id)
-        <div>ここにプロフィール入る</div>
+        <div>{{ $request->profile_content }}</div>
            {!! link_to_route('users.profile_edit', 'プロフィールを編集する',['id' => Auth::id()],["class"=>"nav-link btn btn-primary"]) !!}
         @endif
         </div>
